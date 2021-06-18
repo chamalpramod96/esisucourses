721ee1d222ab8598257ab58f83611fb5c7b74004
chamalpramod96 <chamalps21@gmail.com>
2021-06-19 02:10:54 +0530

    add homepage coursespage and fontstyles

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -12,4 +12,20 @@ export class CourseComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  course = [
+    { 'id': 1, 'name': 'Learn Arduino', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course01.png' },
+    { 'id': 2, 'name': 'Learn AWS', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course02.png' },
+    { 'id': 3, 'name': 'Learn C++', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course03.png' },
+    { 'id': 4, 'name': 'Learn CSS', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course04.png' },
+    { 'id': 5, 'name': 'Learn Java', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course05.png' },
+    { 'id': 6, 'name': 'Learn jQuery', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course06.png' },
+    { 'id': 7, 'name': 'Learn JavaScript', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course07.png' },
+    { 'id': 8, 'name': 'Learn HTML', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course08.png' },
+    { 'id': 9, 'name': 'Learn SQL', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course09.jpeg' },
+    { 'id': 10, 'name': 'Learn Python', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since.', 'image': '../../assets/images/course010.png' },
+    { 'id': 9, 'name': 'Learn SE Development', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy.', 'image': '../../assets/images/course011.png' },
+    { 'id': 10, 'name': 'Learn Email marketing', 'description': 'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy.', 'image': '../../assets/images/course012.png' },
+    
+  ]
+
 }
