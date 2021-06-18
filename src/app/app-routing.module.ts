5e880253a08c18b622a679ad010e5aa860c962d7
chamalpramod96 <chamalps21@gmail.com>
2021-06-18 20:28:52 +0530

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent,data:{title:'Home | ESISU'}},
+  { path: 'aboutus', component: AboutusComponent,data:{title:'About Us | ESISU'} },
+  { path: 'contact', component: ContactComponent ,data:{title:'Contact | ESISU'}},
+  { path: 'course', component: CourseComponent,data:{title:'Course | ESISU'} },
+  { path: 'joinnow', component: JoinnowComponent ,data:{title:'Join Now | ESISU'}},
+  { path: 'login', component: LoginComponent,data:{title:'Login | ESISU'} },
+  { path: 'signin', component: SigninComponent ,data:{title:'SignIn | ESISU'}},
+  { path: 'signu', component: SignupComponent,data:{title:'SignUp | ESISU'} }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CourseComponent,
+    JoinnowComponent,
+    AboutusComponent,
+    SignupComponent,
+    SigninComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
